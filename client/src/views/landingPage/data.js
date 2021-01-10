e20dad36d4a64c9a3545ac289e6c43e09ff60cff
Gaurav Jaiswal <gaurav55558@gmail.com>
2021-01-10 17:47:27 +0530

    ravi pic change

--- client/src/views/landingPage/data.js
+++ client/src/views/landingPage/data.js
@@ -27,7 +27,7 @@ export default [
     content:
       "Enthusiastic CSE Student eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of Web development and Software development and training in java for real life development. Motivated to learn, grow and excel in IT industry.",
     media:
-      "https://res.cloudinary.com/devhubimg/image/upload/v1609787709/rv.jpg",
+      "https://res.cloudinary.com/devhubimg/image/upload/v1610280978/rv2.jpg",
     username: "Ravi",
     likes: 444,
   },
